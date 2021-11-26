567c014e38c6ee80e4d75402c4729a269b5bdccb
Diego Dantas <deigo_batista@hotmail.com>
2021-11-26 00:12:22 -0300

    Update adivinhacao.py

--- jogo_adivinhacao/adivinhacao.py
+++ jogo_adivinhacao/adivinhacao.py
@@ -8,7 +8,11 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f'Tentativa {rodada} de {total_de_tentativas}: ')
-    chute = int(input('Digite seu número: '))
+    chute = int(input('Digite um número entre 1 e 100: '))
+
+    if chute < 1 or chute > 100:
+        print('Você deve digitar um número entre 1 e 100!')
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
