75bfe656d8119af4d3a4ca211e7aeb69ef9fc578
Diego Dantas <deigo_batista@hotmail.com>
2021-12-15 00:00:54 -0300

    Update main.py

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -3,30 +3,17 @@
 # parâmetro. A utilização dos símbolos de adição (+) é feita porque não podemos utilizar espaços em branco em URLs. Note que o símbolo “?” não faz parte dos
 # parâmetros, pois é apenas o separador: base?parâmetros.
 
+# nossa url é uma string
+#url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar
+# &quantidade=100"
+#print(url)
 
-# Antes da interrogação (?) é chamada de base da url e depois é chamado de
-# parametros.
+url = 'http://bytebank.com/cambio?moedaOrigem=real'
+print(f'URL: {url}')
 
-# indicando o domínio: https://
-# site: http://bytebank.com
-#  indicando que eu estou na página de cambio =  cambio
-# Abaixo os parametros:
-# indicando que tem uma variavel chamada de moeda origem recebe real:moedaOrigem=real
-# (&), que serve para separar esses parâmetros
-# outra variavel chamada moeda destino que recebe dolar: moedaDestino=dolar
-# (&), que serve para separar esses parâmetros
-# outra variavel que recebe a quantidade: quantidade=100
+interrogacao = url.find('?')
+url_base = url[0:interrogacao]
+print(f'URL base: {url_base}')
 
-# nossa url é um string
-url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-print(url)
-
-url_tamanho = 'bytebank.com/cambio?moedaOrigem=real'
-print(f'Tamanho da URL: {len(url)}')
-
-# Abaixo exemplo de fatiamento
-url_base = url_tamanho[0:19]
-print(url_base)
-
-url_parametro = url_tamanho[20:36]
-print(url_parametro)
+url_parametro = url[interrogacao+1:]
+print(f'URL parametro: {url_parametro}')
